f3f15014e30db4e286a25c8fd1cbe2c7b2e81c12
amanikhe <151717789+amanikhe@users.noreply.github.com>
2023-11-22 21:51:10 +0100

    Update Forum.cy.js

--- Test Fomulaire/cypress/e2e/Forum.cy.js
+++ Test Fomulaire/cypress/e2e/Forum.cy.js
@@ -8,7 +8,7 @@ Given('Je visite l\'URL du formulaire', () => {
 });
 
 When('Je remplis le formulaire avec des données de test', () => {
-  cy.request('GET', 'https://dummyapi.io/data/api/user')
+  cy.request('GET', 'https://dummyapi.io')
     .then((response) => {
       const user = response.body.data[0];
 
